00c835e4a21577d77e50d6a191ad89da6c5ee0e7
pablo <pablo@192.168.7.36>
2023-05-11 20:05:41 +0200

    primer paso

---
+++ ProyectoGitExamen/src/paqueteGitExamen/Clase.java
@@ -0,0 +1,12 @@
+package paqueteGitExamen;
+
+public class Clase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Este es el examen.");
+
+	}
+
+}
